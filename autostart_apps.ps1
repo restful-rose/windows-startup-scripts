41a13273a87f225f9f2dc8ec434a277566026a44
Nicolai Krohg <nick@nve.no>
2025-05-21 16:38:50 +0200

    refactor(keepassxc): use one-password instead of keepass for vera-crypt password retrieval

--- autostart_apps.ps1
+++ autostart_apps.ps1
@@ -2,32 +2,10 @@
 # Secrets management
 # -----------------------------------
 
-$keepassVaultPath = "C:\Users\nick\OneDrive - Norges vassdrags- og energidirektorat\Dokumenter\Conf\NVE_passwords.kdbx"
-$mainPasswordEntryName = "A normal NVE User account"
 $volumePath = "C:\Nick\EncryptedVolume.hc"
-$veraPasswordEntryName = "AAA VeraCrypt"
-
-$maxAttempts = 5
-$mainPassword = ""
-
-for ($i = 1; $i -le $maxAttempts; $i++) {
-    $mainPassword = keep show -a password $keepassVaultPath $mainPasswordEntryName
-    if ($mainPassword -and $mainPassword.Length -gt 0) {
-        Write-Output "Password accepted!"
-        break
-    }
-    Write-Output "Wrong password. Attempt ($i of $maxAttempts)"
-}
-if (-not $mainPassword.Length -gt 0) {
-    Write-Output "No password.. exiting"
-    exit 1
-}
-
-# Start KeePassXC GUI
-$mainPassword | KeePassXC.exe --pw-stdin "$keepassVaultPath"
 
 # Get Vera Crypt password and mount drive
-$veraCryptPassword = $mainPassword | keep show -q -a password $keepassVaultPath $veraPasswordEntryName
+$veraCryptPassword = op read "op://Employee/VeraCrypt/password"
 Write-Output "Mounting encrypted partition..."
 VeraCrypt.exe /v $volumePath /l A /p $veraCryptPassword /q /s
 # Sleep while drive is mounting
