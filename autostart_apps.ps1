d23d3e88707f4be043123713750880e5893f52c7
Nicolai Krohg <nick@nve.no>
2026-05-04 09:12:15 +0200

    go back to using keepass for automation

--- autostart_apps.ps1
+++ autostart_apps.ps1
@@ -12,22 +12,24 @@ $mainPassword = ""
 
 for ($i = 1; $i -le $maxAttempts; $i++) {
     $mainPassword = keep show -a password $keepassVaultPath $mainPasswordEntryName
-    if ($mainPassword -and $mainPassword.Length -gt 0) {
+    if (-not [string]::IsNullOrWhiteSpace($mainPassword)) {
         Write-Output "Password accepted!"
         break
     }
     Write-Output "Wrong password. Attempt ($i of $maxAttempts)"
 }
-if (-not $mainPassword.Length -gt 0) {
+if ([string]::IsNullOrWhiteSpace($mainPassword)) {
     Write-Output "No password.. exiting"
     exit 1
 }
 
 # Start KeePassXC GUI
-$mainPassword | KeePassXC.exe --pw-stdin "$keepassVaultPath"
+#$mainPassword | KeePassXC.exe --pw-stdin "$keepassVaultPath"
+#Start-Process -Filepath "KeePassXC.exe" -ArgumentList "`"$keepassVaultPath`""
+#Write-Output "Started KeePassXC GUI"
 
 # Get Vera Crypt password and mount drive
-$veraCryptPassword = $mainPassword | keep show -q -a password $keepassVaultPath $veraPasswordEntryName
+$veraCryptPassword = $mainPassword | keepassxc-cli.exe show -q -a password $keepassVaultPath $veraPasswordEntryName
 Write-Output "Mounting encrypted partition..."
 VeraCrypt.exe /v $volumePath /l A /p $veraCryptPassword /q /s
 # Sleep while drive is mounting
@@ -40,6 +42,7 @@ Start-Sleep 9
 
 # Start Obsidian
 
+Write-Output "Starting Obisidan"
 $name = "Obsidian"
 # $packageName = Get-AppxPackage | Where-Object { $_.Name -match $name } | Select-Object -ExpandProperty PackageFamilyName
 $packageName = "nve.Obsidian_hwxf5dwcp4wbm"
@@ -48,12 +51,14 @@ Start-Process shell:AppsFolder\$packageName!$name
 
 # Start 202020
 
+Write-Output "Starting 202020"
 $name = "202020"
 # $packageName = Get-AppxPackage | Where-Object { $_.Name -match $name } | Select-Object -ExpandProperty PackageFamilyName
 $packageName = "44686MichaelChen.202020_1xnyq3bbv8tvj"
 Start-Process shell:AppsFolder\$packageName!App
 
+Write-Output "Updating security champion fraction"
 # Update the security champion-fraction with updated information
-Sprint-Report.exe sc
+Sprint-Report.exe sc --use-vault
 # Update background picture with new information
 C:\Nick\Bin\BGInfo\Bginfo.exe "C:\Nick\Bin\BGInfo\nve_background.bgi" /timer:0 /silent /nolicprompt
---
+++ set_no_dismiss_outlook_setting.ps1
@@ -0,0 +1,3 @@
+reg add HKEY_CURRENT_USER\Software\Microsoft\Office\16.0\Outlook\Options\General /v "PONT_STRING" /t REG_SZ /d "53,30," /f
+date > C:\Nick\Source\PowerShell\last_run.txt
+
